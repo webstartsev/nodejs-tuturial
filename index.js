63989ebfcde368e188c26ffc8b353f4fecd872f8
Sergey Startsev <sergey@webstartsev.ru>
2020-03-20 12:37:01 +0300

    server

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const obj = require('./user');
+const http = require('http');
 
-console.log(obj.user);
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end(` 
+      <h1>Form</h1>
+      <form method="post" action="/">
+        <input type="text" name="title"/>
+        <button type="submit">send</button>
+      </form>
+    `);
+  } else if (req.method === 'POST') {
+    const body = [];
 
-obj.sayHello();
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+
+    req.on('data', data => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on('end', () => {
+      const message = body.toString().split('=')[1];
+
+      res.end(`
+        <h1>Ваше сообщение: ${message}</h1>
+      `);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server start...');
+});
