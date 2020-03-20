176aff18fca3b306728b776c852c5dd1ae8e189d
Sergey Startsev <sergey@webstartsev.ru>
2020-03-20 11:09:28 +0300

    ref os

---
+++ refs/os_ref.js
@@ -0,0 +1,16 @@
+const os = require('os');
+
+// Платформа
+console.log(os.platform());
+// Архитектура
+console.log(os.arch());
+// Информация
+console.log(os.cpus());
+// Свободная память
+console.log(os.freemem());
+// Сколько всего памяти
+console.log(os.totalmem());
+// Корневая директория
+console.log(os.homedir());
+// Сколько система работате
+console.log(os.uptime());
