293ec90642640c72ce5ab4811c68db718bec5946
Sergey Startsev <sergey@webstartsev.ru>
2020-03-20 10:40:01 +0300

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+const obj = require('./user');
+
+console.log(obj.user);
+
+obj.sayHello();
---
+++ user.js
@@ -0,0 +1,11 @@
+const user = {
+  name: 'Sergey',
+  age: 31
+};
+
+module.exports = {
+  user,
+  sayHello() {
+    console.log('Hello');
+  }
+};
