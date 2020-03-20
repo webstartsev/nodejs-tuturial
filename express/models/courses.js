a60cc12b753ba5d1b80a51ab18dadffb78bc20b5
Sergey Startsev <sergey@webstartsev.ru>
2020-03-20 17:41:15 +0300

    add model Course & save to json

---
+++ express/models/courses.js
@@ -0,0 +1,50 @@
+const nanoid = require('nanoid');
+const fs = require('fs');
+const path = require('path');
+
+class Course {
+  constructor(title, price, img) {
+    this.title = title;
+    this.price = price;
+    this.img = img;
+    this.id = nanoid();
+  }
+
+  toJSON() {
+    return {
+      title: this.title,
+      price: this.price,
+      img: this.img,
+      id: this.id
+    };
+  }
+
+  async save() {
+    const courses = await Course.getAll();
+    courses.push(this.toJSON());
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        err => {
+          if (err) reject(err);
+
+          resolve();
+        }
+      );
+    });
+  }
+
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path.join(__dirname, '..', 'data', 'courses.json'), 'utf-8', (err, content) => {
+        if (err) reject(err);
+
+        resolve(JSON.parse(content));
+      });
+    });
+  }
+}
+
+module.exports = Course;
