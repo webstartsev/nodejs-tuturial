b2a10fb8faf8f3ed1d5763e85ffa7ed4d0e126ea
Sergey Startsev <sergey@webstartsev.ru>
2020-03-20 12:46:41 +0300

    add views

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     res.writeHead(200, {
       'Content-Type': 'text/html'
     });
-    res.end(` 
-      <h1>Form</h1>
-      <form method="post" action="/">
-        <input type="text" name="title"/>
-        <button type="submit">send</button>
-      </form>
-    `);
+
+    if (req.url === '/') {
+      fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
+        if (err) throw err;
+
+        res.end(content);
+      });
+    } else if (req.url === '/about') {
+      fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
+        if (err) throw err;
+
+        res.end(content);
+      });
+    }
   } else if (req.method === 'POST') {
     const body = [];
 
