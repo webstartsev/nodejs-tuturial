020f99948e2164d765aaf3387eefd579ca3e3471
Sergey Startsev <sergey@webstartsev.ru>
2020-03-23 11:26:42 +0300

    editing course

--- express/models/courses.js
+++ express/models/courses.js
@@ -36,6 +36,25 @@ class Course {
     });
   }
 
+  static async update(course) {
+    const courses = await Course.getAll();
+
+    const idx = courses.findIndex(c => c.id === course.id);
+    courses[idx] = course;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        err => {
+          if (err) reject(err);
+
+          resolve();
+        }
+      );
+    });
+  }
+
   static getAll() {
     return new Promise((resolve, reject) => {
       fs.readFile(path.join(__dirname, '..', 'data', 'courses.json'), 'utf-8', (err, content) => {
