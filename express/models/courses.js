869e6267ad3cbb0f7de5576c34b7c2f0620b2e57
Sergey Startsev <sergey@webstartsev.ru>
2020-03-23 15:55:28 +0300

    update course model

--- express/models/courses.js
+++ express/models/courses.js
@@ -1,74 +1,15 @@
-const nanoid = require('nanoid');
-const fs = require('fs');
-const path = require('path');
+const { Schema, model } = require('mongoose');
 
-class Course {
-  constructor(title, price, img) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = nanoid();
-  }
+const course = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  img: String
+});
 
-  toJSON() {
-    return {
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id
-    };
-  }
-
-  async save() {
-    const courses = await Course.getAll();
-    courses.push(this.toJSON());
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses),
-        err => {
-          if (err) reject(err);
-
-          resolve();
-        }
-      );
-    });
-  }
-
-  static async update(course) {
-    const courses = await Course.getAll();
-
-    const idx = courses.findIndex(c => c.id === course.id);
-    courses[idx] = course;
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses),
-        err => {
-          if (err) reject(err);
-
-          resolve();
-        }
-      );
-    });
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(path.join(__dirname, '..', 'data', 'courses.json'), 'utf-8', (err, content) => {
-        if (err) reject(err);
-
-        resolve(JSON.parse(content));
-      });
-    });
-  }
-
-  static async getById(id) {
-    const courses = await Course.getAll();
-    return courses.find(course => course.id === id);
-  }
-}
-
-module.exports = Course;
+module.exports = model('Course', course);
