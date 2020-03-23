969f483d803a8357816b639e128d7eb28866ee96
Sergey Startsev <sergey@webstartsev.ru>
2020-03-23 12:54:43 +0300

    dinamic update template

--- express/public/app.js
+++ express/public/app.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price =>
+  new Intl.NumberFormat('ru-RU', {
     currency: 'rub',
     style: 'currency'
-  }).format(node.textContent);
+  }).format(price);
+
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = toCurrency(node.textContent);
 });
 
 const $card = document.querySelector('#card');
@@ -16,7 +19,27 @@ if ($card) {
         method: 'delete'
       })
         .then(res => res.json())
-        .then(card => console.log('card: ', card));
+        .then(card => {
+          if (card.courses.length) {
+            const html = card.courses
+
+              .map(c => {
+                return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td><button class="btn btn-small js-remove" data-id="${c.id}">Удалить</button></td>
+              </tr>
+              `;
+              })
+              .join('');
+
+            $card.querySelector('tbody').innerHTML = html;
+            $card.querySelector('.price').textContent = toCurrency(card.price);
+          } else {
+            $card.innerHTML = `<p>Корзина пуста</p>`;
+          }
+        });
     }
   });
 }
